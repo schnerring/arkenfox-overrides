9d0ba2c79f6ed3f58cbd124703e5a5ddbf5d1c04
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2022-04-05 02:29:06 +0200

    Remove XOriginTrimmingPolicy override

--- user-overrides.js
+++ user-overrides.js
@@ -1,6 +1,5 @@
-// Allow cross-domain logins (e.g. Atlassian)
+// 1601: Allow cross-domain logins (e.g. Atlassian)
 user_pref("network.http.referer.XOriginPolicy", 0);
-user_pref("network.http.referer.XOriginTrimmingPolicy", 2);
 
 // 0801: Enable search from urlbar
 user_pref("keyword.enabled", true);
