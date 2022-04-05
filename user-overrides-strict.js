2a2922e39bda6d82f16de2844ef46afb32bd8f69
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2022-04-05 02:08:32 +0200

    Remove outdated overrides

--- user-overrides-strict.js
+++ user-overrides-strict.js
@@ -4,19 +4,3 @@ user_pref("network.http.referer.XOriginTrimmingPolicy", 2);
 
 // 0801: Enable search from urlbar
 user_pref("keyword.enabled", true);
-
-// See also: https://www.privacytools.io/browsers/#about_config
-// This is Mozilla's new built-in tracking protection. One of it's benefits is
-// blocking tracking (i.e. Google Analytics) on privileged pages where add-ons
-// that usually do that are disabled.
-user_pref("privacy.trackingprotection.enabled", true);
-
-// See also: https://www.privacytools.io/browsers/#webrtc
-// Make absolutely sure every single WebRTC-related setting is disabled.
-user_pref("media.peerconnection.turn.disable", true);
-user_pref("media.peerconnection.use_document_iceservers", false);
-user_pref("media.peerconnection.video.enabled", false);
-user_pref("media.peerconnection.identity.timeout", 1);
-
-// Disable website access to clipboard events/content
-user_pref("dom.event.clipboardevents.enabled", false);
--- user-overrides.js
+++ user-overrides.js
@@ -5,19 +5,5 @@ user_pref("network.http.referer.XOriginTrimmingPolicy", 2);
 // 0801: Enable search from urlbar
 user_pref("keyword.enabled", true);
 
-// See also: https://www.privacytools.io/browsers/#about_config
-// This is Mozilla's new built-in tracking protection. One of it's benefits is
-// blocking tracking (i.e. Google Analytics) on privileged pages where add-ons
-// that usually do that are disabled.
-user_pref("privacy.trackingprotection.enabled", true);
-
-// See also: https://www.privacytools.io/browsers/#webrtc
-// Make absolutely sure every single WebRTC-related setting is disabled.
-user_pref("media.peerconnection.turn.disable", true);
-user_pref("media.peerconnection.use_document_iceservers", false);
-user_pref("media.peerconnection.video.enabled", false);
-user_pref("media.peerconnection.identity.timeout", 1);
-
-// Store cookies and history
-user_pref("privacy.clearOnShutdown.cookies", false);
+// 2811: Keep history
 user_pref("privacy.clearOnShutdown.history", false);
