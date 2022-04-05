d5ae9f0c93b8570a68efd907b03ec138e91181fe
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2022-04-05 02:40:10 +0200

    Set XOriginPolicy to default

--- user-overrides-strict.js
+++
@@ -1,5 +0,0 @@
-// 1601: Allow cross-domain logins (e.g. Atlassian)
-user_pref("network.http.referer.XOriginPolicy", 0);
-
-// 0801: Enable search from urlbar
-user_pref("keyword.enabled", true);
--- user-overrides.js
+++ user-overrides.js
@@ -1,5 +1,5 @@
 // 1601: Allow cross-domain logins (e.g. Atlassian)
-user_pref("network.http.referer.XOriginPolicy", 0);
+//user_pref("network.http.referer.XOriginPolicy", 0);
 
 // 0801: Enable search from urlbar
 user_pref("keyword.enabled", true);
