4a2dbe7f445f726fb1b1934e2a502fdd898c79be
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2022-08-02 23:53:32 +0200

    Re-configure arkenfox

--- user-overrides.js
+++ user-overrides.js
@@ -1,11 +1,15 @@
-// 1601: Allow cross-domain logins (e.g. Atlassian)
-//user_pref("network.http.referer.XOriginPolicy", 0);
+// URL bar search
+user_pref("keyword.enabled", true);                   // 0801: enabled urlbar search
+user_pref("browser.search.suggest.enabled", false);   // 0804: live search suggestions
 
-// 0801: Enable search from urlbar
-user_pref("keyword.enabled", true);
+// Session Restore
+user_pref("browser.startup.page", 3);                 // 0102: enable session restore
+user_pref("privacy.clearOnShutdown.history", false);  // 2811: don't clear history on close
+user_pref("privacy.cpd.history", false);              // 2820: untick history in Ctrl-Shift-Del dialog
 
-// 2811: Keep history
-user_pref("privacy.clearOnShutdown.history", false);
+// Other
+user_pref("signon.rememberSignons", false);           // 5003: disable saving passwords
+user_pref("network.file.disable_unc_paths", true);    // 0703: enalbe UNC paths
 
-// 5003: disable saving passwords
-user_pref("signon.rememberSignons", false);
+// TODO
+//user_pref("media.peerconnection.enabled", false);     // 2001: disable WebRTC
