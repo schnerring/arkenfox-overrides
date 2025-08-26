545abf54bda9cf07cdc043fee7c0313827923f4c
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2025-08-27 01:31:48 +0200

    fix: location bar live search suggestions

--- user-overrides.js
+++ user-overrides.js
@@ -4,8 +4,9 @@ user_pref("privacy.clearOnShutdown_v2.browsingHistoryAndDownloads", false);
 user_pref("privacy.clearSiteData.browsingHistoryAndDownloads", false);
 user_pref("privacy.clearHistory.browsingHistoryAndDownloads", false);
 
-// enable live search suggestions
+// enable location bar live search suggestions
 user_pref("browser.search.suggest.enabled", true);
+user_pref("browser.urlbar.suggest.searches", true);
 
 // disable saving passwords
 user_pref("signon.rememberSignons", false);
