d1232fbd398fcf1e49758e11109511a0ba37ebe6
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2025-09-17 15:16:56 +0200

    fix: disable url bar search suggestions

--- user-overrides.js
+++ user-overrides.js
@@ -4,9 +4,9 @@ user_pref("privacy.clearOnShutdown_v2.browsingHistoryAndDownloads", false);
 user_pref("privacy.clearSiteData.browsingHistoryAndDownloads", false);
 user_pref("privacy.clearHistory.browsingHistoryAndDownloads", false);
 
-// enable location bar live search suggestions
+// enable live search suggestions
 user_pref("browser.search.suggest.enabled", true);
-user_pref("browser.urlbar.suggest.searches", true);
+// user_pref("browser.urlbar.suggest.searches", true);
 
 // disable saving passwords
 user_pref("signon.rememberSignons", false);
